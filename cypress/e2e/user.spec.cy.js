cb33ca9bb3537f186c0f802fdb81ccf2a8530d79
Betinho <betosoares0202@gmail.com>
2025-03-13 16:28:04 -0300

    cenarios

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -11,17 +11,28 @@ describe('Orange HRM Tests', () => {
     SectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
     dashboardGrid: '.orangehrm-dashboard-grid',
     wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    EmployeeField: ".oxd-input--active",
+
   }
 
 
 
-  it('Login com sucesso', () => {
+  it.only('User Update - sucesso', () => {
+
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucesso.username)
     cy.get(selectorsList.passwordField).type(userData.userSucesso.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField)
+    cy.get(selectorsList.lastNameField)
+    cy.get(selectorsList.EmployeeField).eq(5).type('EmployeeFieldTest')
+
   })
   it('Login sem sucesso', () => {
     cy.visit('/auth/login')
